0d75d1a480fb93e15dd38f7395b1e451262f0296
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-18 18:36:49 -0400

    fixed an issue with a bad git pull

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './Card.css';
 import BannerImg from './CardBanner';
 
-<div className="bannerBottom">
-    <p>Get started with React</p>
-    <p>
-      React makes it painless to make interactive UIs. Design simple views
-      for each state in your application.
-    </p>
-    <div>
-    <p className="CardLink">reactjs.org</p>
-    </div>
-</div>
+const CardContent = () => {
+    return (
+      <div className="banner-bottom">
+        <BannerImg />
+        
+        <p className="BottomP">Get started with React</p>
+        <p className="BottomP">
+          React makes it painless to make interactive UI's. Design simple views
+          for each state in your application.
+        </p>
+        
+        <div>
+            <p className="CardLink BottomP">reactjs.org</p>
+        </div>
+       
+         
+        
+      </div>
+    );
+  };
+  export default CardContent;
