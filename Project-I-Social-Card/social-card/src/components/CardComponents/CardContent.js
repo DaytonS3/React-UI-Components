0ee41a11baefb779cd47045efe8dbee13a69709d
Dayton Steinmeyer <45213939+DaytonS3@users.noreply.github.com>
2019-03-18 18:06:35 -0400

    Update CardContent.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -2,22 +2,13 @@ import React from 'react';
 import './Card.css';
 
 
-const CardContent = () => {
-    return (
-      <div className="banner-bottom">
-        <p>Get started with React</p>
-        <p>
-          React makes it painless to make interactive UI's. Design simple views
-          for each state in your application.
-        </p>
-        
-        <div>
-            <p className="CardLink">reactjs.org</p>
-        </div>
-       
-         
-        
-      </div>
-    );
-  };
-  export default CardContent;
+<div className="bannerBottom">
+    <p>Get started with React</p>
+    <p>
+      React makes it painless to make interactive UI's. Design simple views
+      for each state in your application.
+    </p>
+    <div>
+    <p className="CardLink">reactjs.org</p>
+    </div>
+</div>
