fc79b0aad7f20ae8ba2961735ddbc9c14b108e07
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-18 16:58:39 -0400

    finshied Header Component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderImage from "./ImageThumbnail";
+import HeaderTitle from '../HeaderComponents/HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+
+const HeaderContainer = () => {
+    return (
+      <div class="Header">
+        <HeaderImage />
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    );
+  };
+
+  export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+      <div class="header-Title">
+        @Lambda School -
+        03/18/19
+        
+      </div>
+      
+    )
+  };
+
+  export default HeaderTitle;
