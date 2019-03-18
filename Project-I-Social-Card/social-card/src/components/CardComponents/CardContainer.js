f697241fde796807cd9fa2c55312749a4dd43c6f
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-18 18:27:33 -0400

    cleaned up MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Card.css';
-import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = () => {
     return (
       <div className="Card-Container">
-        <CardBanner />
+        
         <CardContent />
       </div>
     );
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Card.css';
-
+import BannerImg from './CardBanner';
 
 const CardContent = () => {
     return (
       <div className="banner-bottom">
-        <p>Get started with React</p>
-        <p>
+        <BannerImg />
+        
+        <p className="BottomP">Get started with React</p>
+        <p className="BottomP">
           React makes it painless to make interactive UI's. Design simple views
           for each state in your application.
         </p>
         
         <div>
-            <p className="CardLink">reactjs.org</p>
+            <p className="CardLink BottomP">reactjs.org</p>
         </div>
        
          
